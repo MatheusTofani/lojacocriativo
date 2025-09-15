9784968a4bab6365e5f63c729f1a851aa9dff6b6
Matheus <matheus271112@gmail.com>
2025-09-15 14:04:37 -0300

    Catalogo Att

--- importExcel.js
+++ importExcel.js
@@ -2,7 +2,7 @@ const admin = require("firebase-admin");
 const XLSX = require("xlsx");
 
 // importa o service account
-const serviceAccount = require("./lojacocriativo-1c233-firebase-adminsdk-fbsvc-0b1941976a.json");
+const serviceAccount = require("./lojacocriativo-1c233-firebase-adminsdk-fbsvc-db87aefdca.json");
 
 // inicializa o Firebase Admin com as credenciais
 admin.initializeApp({
